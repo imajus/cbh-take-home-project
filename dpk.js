0bb6243e1d983ac3659b9c05177f18842c6d0b06
Denis Perov <denis.perov@gmail.com>
2023-03-01 00:01:03 +0300

    Use a refactored version by ChatGPT

--- dpk.js
+++ dpk.js
@@ -3,21 +3,15 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  if (event) {
-    let data;
-    if (event.partitionKey) {
-      data = event.partitionKey;
-      if (typeof data !== "string") {
-        data = JSON.stringify(data);
-      }
-      if (data.length <= MAX_PARTITION_KEY_LENGTH) {
-        return data;
-      }
-    } else {
-      data = JSON.stringify(event);
-    }
-    return crypto.createHash("sha3-512").update(data).digest("hex");
-  } else {
+
+  if (!event) {
     return TRIVIAL_PARTITION_KEY;
   }
+
+  let data = event.partitionKey ?? JSON.stringify(event);
+  data = typeof data === "string" ? data : JSON.stringify(data);
+
+  return data.length <= MAX_PARTITION_KEY_LENGTH
+    ? data
+    : crypto.createHash("sha3-512").update(data).digest("hex");
 };
